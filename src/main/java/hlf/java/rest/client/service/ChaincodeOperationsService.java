7a30703504e67aa59315f26202e01f09ba82ef81
Puneet Joshi <puneet.joshi@walmart.com>
2022-03-13 18:01:27 +0530

    Addition of API to get packageId for a chaincode
    - REST API and service to get current packageId installed in the peers
    
    Signed-off-by: Puneet Joshi <puneet.joshi@walmart.com>

--- src/main/java/hlf/java/rest/client/service/ChaincodeOperationsService.java
+++ src/main/java/hlf/java/rest/client/service/ChaincodeOperationsService.java
@@ -5,10 +5,36 @@ import hlf.java.rest.client.model.ChaincodeOperationsType;
 
 public interface ChaincodeOperationsService {
 
+  /**
+   * Perform chaincode operations passed in the arguments.
+   *
+   * @param networkName the network name
+   * @param chaincodeOperationsModel the chaincode operations model
+   * @param operationsType the operations type
+   * @return the transactionId
+   */
   String performChaincodeOperation(
       String networkName,
       ChaincodeOperations chaincodeOperationsModel,
       ChaincodeOperationsType operationsType);
 
+  /**
+   * Gets current sequence for the channel and chaincode.
+   *
+   * @param networkName the network name
+   * @param chaincodeName the chaincode name
+   * @param chaincodeVersion the chaincode version
+   * @return the current sequence
+   */
   String getCurrentSequence(String networkName, String chaincodeName, String chaincodeVersion);
+
+  /**
+   * Gets current package id for the passed channel and chaincode.
+   *
+   * @param networkName the network name
+   * @param chaincodeName the chaincode name
+   * @param chaincodeVersion the chaincode version
+   * @return the current package id
+   */
+  String getCurrentPackageId(String networkName, String chaincodeName, String chaincodeVersion);
 }
