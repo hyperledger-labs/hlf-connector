5ab3aaa1af75227384075a2ad0f0bd37666996a4
“Nithin <nithin.pankaj@walmartlabs.com>
2022-10-25 10:55:09 +0530

    Add SSLAuthFilesCreationHelper for creating SSL files from Base64 in Appconfig
    Signed-off-by: “Nithin <nithin.pankaj@walmartlabs.com>

--- src/main/java/hlf/java/rest/client/service/ConfigHandlerService.java
+++
@@ -1,7 +0,0 @@
-package hlf.java.rest.client.service;
-
-import hlf.java.rest.client.model.ClientResponseModel;
-
-public interface ConfigHandlerService {
-  ClientResponseModel updateConfiguration(byte[] newConfigBytes);
-}
