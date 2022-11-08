debaaed84d605665f1efa4848e7b7af59bd5924a
Abhay Kishore <abhay.kishore@walmart.com>
2022-11-08 16:15:54 +0530

    Changes to fix failing integration test
    Signed-off-by: Abhay Kishore <abhay.kishore@walmart.com>

--- src/test/java/hlf/java/rest/client/IT/SmartContractIT.java
+++ src/test/java/hlf/java/rest/client/IT/SmartContractIT.java
@@ -8,16 +8,21 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import hlf.java.rest.client.model.ChaincodeOperations;
 import hlf.java.rest.client.model.ChaincodeOperationsType;
+import hlf.java.rest.client.model.ChannelOperationRequest;
+import hlf.java.rest.client.model.MSPDTO;
+import hlf.java.rest.client.model.Orderer;
 import hlf.java.rest.client.service.ChaincodeOperationsService;
+import hlf.java.rest.client.service.ChannelService;
 import hlf.java.rest.client.service.HFClientWrapper;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import org.hyperledger.fabric.gateway.Gateway;
-import org.hyperledger.fabric.gateway.Network;
+import org.apache.commons.io.FileUtils;
 import org.hyperledger.fabric.sdk.Channel;
 import org.hyperledger.fabric.sdk.HFClient;
 import org.hyperledger.fabric.sdk.LifecycleChaincodePackage;
@@ -41,9 +46,9 @@ public class SmartContractIT {
 
   @Autowired ChaincodeOperationsService chaincodeOperationsService;
   @Autowired private HFClientWrapper hfClientWrapper;
-  @Autowired private Gateway gateway;
-  private static final String CHANNEL_NAME = "test1";
-  private static final String CHAINCODE_NAME = "example-cc-1";
+  @Autowired ChannelService channelService;
+  private static final String CHANNEL_NAME = "sc-test";
+  private static final String CHAINCODE_NAME = "example-cc_1";
   private static final String CHAINCODE_VERSION = "1";
 
   @Test
@@ -51,8 +56,8 @@ public class SmartContractIT {
   public void installChaincodeTest()
       throws InvalidArgumentException, IOException, ProposalException {
 
-    Network network = gateway.getNetwork(CHANNEL_NAME);
-    Channel channel = network.getChannel();
+    createAndJoinChannel();
+    Channel channel = hfClientWrapper.getHfClient().getChannel(CHANNEL_NAME);
 
     LifecycleChaincodePackage lifecycleChaincodePackage =
         LifecycleChaincodePackage.fromSource(
@@ -65,6 +70,61 @@ public class SmartContractIT {
         lifecycleInstallChaincode(
             hfClientWrapper.getHfClient(), channel.getPeers(), lifecycleChaincodePackage);
     assertNotNull(chaincodePackageID);
+  }
+
+  private void createAndJoinChannel() {
+    ChannelOperationRequest channelOperationRequest = new ChannelOperationRequest();
+    channelOperationRequest.setChannelName(CHANNEL_NAME);
+    channelOperationRequest.setConsortiumName("SampleConsortium");
+    try {
+      Orderer orderer = new Orderer();
+      orderer.setName("orderer1");
+      orderer.setGrpcUrl("grpc://localhost:7050");
+      orderer.setCertificate(
+          FileUtils.readFileToString(
+              Paths.get(
+                      "src/test/java/fabricSetup/e2e-2Orgs/v2.1/crypto-config/ordererOrganizations/example.com/msp/admincerts/Admin@example.com-cert.pem")
+                  .toFile()));
+      List<Orderer> ordererList = new ArrayList<>();
+      ordererList.add(orderer);
+      channelOperationRequest.setOrderers(ordererList);
+
+      hlf.java.rest.client.model.Peer peer = new hlf.java.rest.client.model.Peer();
+      peer.setName("peer1");
+      peer.setMspid("Org1MSP");
+      peer.setGrpcUrl("grpc://localhost:7051");
+      String admincert =
+          FileUtils.readFileToString(
+              Paths.get(
+                      "src/test/java/fabricSetup/e2e-2Orgs/v2.1/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/admincerts/Admin@org1.example.com-cert.pem")
+                  .toFile());
+      String cacert =
+          FileUtils.readFileToString(
+              Paths.get(
+                      "src/test/java/fabricSetup/e2e-2Orgs/v2.1/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/cacerts/ca.org1.example.com-cert.pem")
+                  .toFile());
+      peer.setCertificate(cacert);
+
+      MSPDTO mspdto = new MSPDTO();
+      List<String> rootCerts = new ArrayList<>();
+      rootCerts.add(cacert);
+      mspdto.setRootCerts(rootCerts);
+      List<String> tlsRootCerts = new ArrayList<>();
+      tlsRootCerts.add(cacert);
+      mspdto.setTlsRootCerts(tlsRootCerts);
+      mspdto.setAdminOUCert(admincert);
+      mspdto.setClientOUCert(admincert);
+      mspdto.setPeerOUCert(cacert);
+      peer.setMspDTO(mspdto);
+
+      List<hlf.java.rest.client.model.Peer> peerList = new ArrayList<>();
+      peerList.add(peer);
+      channelOperationRequest.setPeers(peerList);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    channelService.createChannel(channelOperationRequest);
+    channelService.joinChannel(channelOperationRequest);
   }
 
   private String lifecycleInstallChaincode(
@@ -125,22 +185,27 @@ public class SmartContractIT {
   @Order(3)
   public void getCurrentPackageIdTest() {
 
+    // chaincode version changed to satisfy the new condition for fetching label
     String packageId =
         chaincodeOperationsService.getCurrentPackageId(
-            CHANNEL_NAME, CHAINCODE_NAME, CHAINCODE_VERSION);
+            CHANNEL_NAME,
+            CHAINCODE_NAME.substring(0, CHAINCODE_NAME.length() - 2),
+            CHAINCODE_VERSION);
     assertNotNull(packageId);
   }
 
   @Test
   @Order(4)
-  public void approveChaincodeTest() {
+  public void approveChaincodeTest() throws InvalidArgumentException, ProposalException {
 
     String sequence =
         chaincodeOperationsService.getCurrentSequence(
             CHANNEL_NAME, CHAINCODE_NAME, CHAINCODE_VERSION);
     String packageId =
         chaincodeOperationsService.getCurrentPackageId(
-            CHANNEL_NAME, CHAINCODE_NAME, CHAINCODE_VERSION);
+            CHANNEL_NAME,
+            CHAINCODE_NAME.substring(0, CHAINCODE_NAME.length() - 2),
+            CHAINCODE_VERSION);
 
     ChaincodeOperations chaincodeOperations =
         ChaincodeOperations.builder()
@@ -157,7 +222,7 @@ public class SmartContractIT {
   }
 
   @Test
-  @Order(5)
+  @Order(6)
   public void commitChaincodeTest() {
 
     String sequence =
@@ -165,7 +230,9 @@ public class SmartContractIT {
             CHANNEL_NAME, CHAINCODE_NAME, CHAINCODE_VERSION);
     String packageId =
         chaincodeOperationsService.getCurrentPackageId(
-            CHANNEL_NAME, CHAINCODE_NAME, CHAINCODE_VERSION);
+            CHANNEL_NAME,
+            CHAINCODE_NAME.substring(0, CHAINCODE_NAME.length() - 2),
+            CHAINCODE_VERSION);
 
     ChaincodeOperations chaincodeOperations =
         ChaincodeOperations.builder()
@@ -182,7 +249,7 @@ public class SmartContractIT {
   }
 
   @Test
-  @Order(6)
+  @Order(5)
   public void getApprovedOrganizationsTest() {
 
     String sequence =
@@ -193,7 +260,7 @@ public class SmartContractIT {
         ChaincodeOperations.builder()
             .chaincodeName(CHAINCODE_NAME)
             .chaincodeVersion(CHAINCODE_VERSION)
-            .sequence(Long.parseLong(sequence) + 1)
+            .sequence(Long.parseLong(sequence))
             .initRequired(false)
             .build();
     Set<String> organizationSet =
