c20e17fad75a046581f9856b47b481f98e5e3457
“Nithin <nithin.pankaj@walmartlabs.com>
2023-04-10 09:29:28 +0530

    Expose conditional beans for emitting custom kafka metrics
    Signed-off-by: “Nithin <nithin.pankaj@walmartlabs.com>

---
+++ src/main/java/hlf/java/rest/client/config/CustomMetricsConfig.java
@@ -0,0 +1,23 @@
+package hlf.java.rest.client.config;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomMetricsConfig {
+
+  @Bean
+  @ConditionalOnProperty(value = "metrics.custom.kafka.enabled", havingValue = "true")
+  public Counter customKafkaSuccessCounter(MeterRegistry meterRegistry) {
+    return meterRegistry.counter("kafka.messages.processed.messages");
+  }
+
+  @Bean
+  @ConditionalOnProperty(value = "metrics.custom.kafka.enabled", havingValue = "true")
+  public Counter customKafkaFailureCounter(MeterRegistry meterRegistry) {
+    return meterRegistry.counter("kafka.messages.failed.messages");
+  }
+}
