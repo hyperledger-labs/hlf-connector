2c3460baf1eef92d67da078db2a474636059ddce
“Nithin <nithin.pankaj@walmartlabs.com>
2023-06-22 21:56:55 +0530

    New Listener method for Contract events & deprecate the old listener
    Signed-off-by: “Nithin <nithin.pankaj@walmartlabs.com>

--- src/main/java/hlf/java/rest/client/listener/ChaincodeEventListener.java
+++ src/main/java/hlf/java/rest/client/listener/ChaincodeEventListener.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnProperty(prefix = "fabric.events", name = "enable", havingValue = "true")
 @RefreshScope
-public class CustomChaincodeEventListener {
+public class ChaincodeEventListener {
 
   @Autowired(required = false)
   private EventPublishService eventPublishService;
