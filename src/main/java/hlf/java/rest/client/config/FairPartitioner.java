fb11e5a5c05b1064f9ab5f4f2927ef14157b5641
Nithin <nithin.pankaj@walmart.com>
2026-03-13 12:31:12 +0530

    Logging improvements - custom partitioner
    Signed-off-by: Nithin <nithin.pankaj@walmart.com>

--- src/main/java/hlf/java/rest/client/config/FairPartitioner.java
+++ src/main/java/hlf/java/rest/client/config/FairPartitioner.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
 
+@Slf4j
 public class FairPartitioner implements Partitioner {
 
   private final ConcurrentHashMap<String, AtomicInteger> topicCounters = new ConcurrentHashMap<>();
@@ -20,6 +22,13 @@ public class FairPartitioner implements Partitioner {
       String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
 
     List<PartitionInfo> availablePartitions = cluster.availablePartitionsForTopic(topic);
+
+    for (PartitionInfo partitionInfo : availablePartitions) {
+      log.info("Available partition: {} for topic: {}", partitionInfo.partition(), topic);
+    }
+
+    log.info("Total available partitions {}", availablePartitions.size());
+
     List<PartitionInfo> partitionsToUse =
         (availablePartitions != null && !availablePartitions.isEmpty())
             ? availablePartitions
